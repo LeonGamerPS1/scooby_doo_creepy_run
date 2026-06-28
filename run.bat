79143db12ffd220535504f7b4a8fe0ee5396f349
chungus <chungus@big.com>
2026-06-28 14:52:15 +0200

    fix run command :wilted_rose:

--- run.bat
+++ run.bat
@@ -1,3 +1,3 @@
 @echo off
 make
-"D:\citra-windows-msvc-20240303-0ff3440_nightly\citra-windows-msvc-20240303-0ff3440\citra-qt.exe" .\scooby_doo_creepy_run.3dsx
+start "" "D:\citra-windows-msvc-20240303-0ff3440_nightly\citra-windows-msvc-20240303-0ff3440\citra-qt.exe" ".\scooby_doo_creepy_run.3dsx"
